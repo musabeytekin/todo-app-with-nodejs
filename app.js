6151756f1fdb4cd4d14aec44afb4dd3504bc6fe2
musabeytekin <02190201003@ogr.inonu.edu.tr>
2022-10-27 21:31:03 +0300

    add new task function

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var items = [];
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs")
+
+const getDayName = (date) => {
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[date.getDay()];
+}
 
 app.get("/", (req, res) => {
     var today = new Date();
-    var currentDay = today.getDay();
+    res.render("list", {dayName: getDayName(today), items: items});
+})
 
-    currentDay === 6 || currentDay === 0 ? res.sendFile(__dirname + "/weekend.html") : res.sendFile(__dirname + "/weekday.html");
-
+app.post("/", (req, res) => {
+    var item = req.body.newTask;
+    items.push(item);
+    res.redirect("/")
 })
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
 })
+
